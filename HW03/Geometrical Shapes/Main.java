4474b8d9810642668c71f69ab53b1ce92f2353e0
kyle <44934418+kwsantiago@users.noreply.github.com>
2019-10-17 12:20:51 -0400

    Update Main.java

--- HW03/Geometrical Shapes/Main.java
+++ HW03/Geometrical Shapes/Main.java
@@ -1,8 +1,14 @@
 public class Main {   
        
    public static void main(String[] args) {
-      Shape shape1 = new Shape();
-      System.out.println(shape1.toString());
+      Circle circle = new Circle("red", true, 5.0);
+      System.out.println(circle.toString());
+      Rectangle rectangle = new Rectangle("blue", false, 6.0, 12.0);
+      System.out.println(circle.toString());
+      Cube cube = new Cube("cyan", false, 4.0);
+      System.out.println(cube.toString());
+      Sphere sphere = new Sphere("magenta", true, 3.0);
+      System.out.println(sphere.toString());
    }// end of main
 
 }// end of class
