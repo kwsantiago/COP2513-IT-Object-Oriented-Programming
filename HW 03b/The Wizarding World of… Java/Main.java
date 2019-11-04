9cefd85bc3a3dc7e56957c0260f84931323c70da
Kyle Santiago <kwsantiago@mail.usf.edu>
2019-11-04 18:20:15 -0500

    Spells

--- HW 03b/The Wizarding World of… Java/Main.java
+++ HW 03b/The Wizarding World of… Java/Main.java
@@ -1,18 +1,29 @@
 public class Main{
+public static void main(String[] args){
   String[] a = {"fireball","sandstorm","earthen hand","word of death"};
-  String[] a = {"prismatic orb","fireball","cloudkill","word of death"};
-  SpellBook book1 = new SpellBook(a);
-  SpellBook book2 = new SpellBook(b);
+  String[] b = {"prismatic orb","fireball","cloudkill","word of death"};
+  SpellBook book1 = new SpellBook();
+  SpellBook book2 = new SpellBook();
+  int sizeA = a.length;
+  int sizeB = b.length;
+      for(int i = 0;i<sizeA;i++){
+         book1.add(i,a[i]);
+         }
+      for(int i = 0;i<sizeB;i++){
+         book2.add(i,b[i]);
+         }
+         System.out.println(book1.toString());
   book1.inventSpell();
   book2.inventSpell();
   book1.addAfter("fireball","snowball");
   book1.forgetSpell("sandstorm");
-  book1.castSpell(book2.counterSpell());
-  book1.castSpell(book2.counterSpell());
-  book1.castSpell(book2.counterSpell());
-  book2.castSpell(book1.counterSpell());
-  book2.castSpell(book1.counterSpell());
-  book2.castSpell(book1.counterSpell());
+  book1.counterSpell(book2.castSpell());
+  book1.counterSpell(book2.castSpell());
+  book1.counterSpell(book2.castSpell());
+  book2.counterSpell(book1.castSpell());
+  book2.counterSpell(book1.castSpell());
+  book2.counterSpell(book1.castSpell());
   System.out.println(book1.toString());
   System.out.println(book2.toString());
+  }// end main
 }//end class
--- HW 03b/The Wizarding World of… Java/SpellBook.java
+++ HW 03b/The Wizarding World of… Java/SpellBook.java
@@ -1,38 +1,79 @@
 /**
-* Kyle Santiago 11/01/2019 
+Kyle Santiago 11/01/2019 
 **/
 import java.util.ArrayList;
 import java.util.Random;
 public class SpellBook extends ArrayList<String>{
-   Random object;
+   Random random;
+   public ArrayList<String> SpellBook;
    
    SpellBook(){
       super();
-      object = new Random();
+      SpellBook = new ArrayList<String>();
+      random = new Random();
    }// end no-arg constructor
    
+   public void add(int i, String s){
+      SpellBook.add(i,s);
+   }// end add
+   
    public void addAfter(String s1, String s2){
-      SpellBook.add(s1,s2);
+      int i;
+      int size = SpellBook.size();
+      for(i = 0;i<size;i++){
+         if(SpellBook.get(i)==s1){
+            break;
+            }
+      }// end for
+      SpellBook.add(i,s2);
    }// end addAfter
    
-   public void counterSpell(){
-      
+   public void counterSpell(String s1){
+      int i;
+      int size = SpellBook.size();
+      for(i = 0;i<size;i++){
+         if(SpellBook.get(i)==s1){
+            System.out.println("turn to page " + i);
+            for(int j = SpellBook.get(i).length();j>=0;i--){
+            System.out.print(SpellBook.get(i).charAt(j));
+            }
+            break;
+            }
+      }// end for
+      System.out.println("dodge");
    }// end counterSpell
    
    public void inventSpell(){
-      
+      int r1 = random.nextInt(SpellBook.size());
+      int r2 = random.nextInt(SpellBook.size());
+      String s1 = SpellBook.get(r1) + SpellBook.get(r2);
+      SpellBook.add(s1);
    }// end inventSpell
    
    public String castSpell(){
-      
+      int r = random.nextInt(SpellBook.size());
+      return SpellBook.get(r - 1);
    }// end castSpell
    
-   public void forgetSpell(){
-      
+   public void forgetSpell(String s1){
+      int i;
+      int size = SpellBook.size();
+      for(i = 0;i<size;i++){
+         if(SpellBook.get(i)==s1){
+            SpellBook.remove(i);
+            break;
+            }
+      }// end for
    }// end forgetSpell
    
    public String toString(){
-      return "Page 1:\n" + SpellBook();
+      int i;
+      int size = SpellBook.size();
+      String tmp = "";
+      for(i = 0;i<size;i++){
+         tmp += "Page " + (i+1) + " " + SpellBook.get(i) + " \n";
+            }// end for
+      return tmp;
    }// end toString
 
 }// end calss
