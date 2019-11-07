76614ca62f9d118f23546a856c596cf9f8a4db73
kyle <44934418+kwsantiago@users.noreply.github.com>
2019-11-06 23:26:49 -0500

    Create Main.java

---
+++ HW 03b/Surveying Students/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+  public static void main(String[] args){
+    ArrayList<String> set1Names;
+    set1Names = new ArrayList<String>;
+    set1Names.add("Bob Scott");
+    set1Names.add("Maria Robinson");
+    set1Names.add("John Stones");
+    set1Names.add("Angela Johnson");
+    StudentSet set1 = new StudentSet(set1Names,set1Ages,set1FavClasses);
+  }// end main
+}// end class
