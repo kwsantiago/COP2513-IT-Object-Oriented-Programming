f1379f6df13a8c5b7d67a3515a730a037dea74ff
Kyle Santiago <kwsantiago@mail.usf.edu>
2019-11-12 19:57:25 -0500

    Update Surveying Student

--- HW 03b/Surveying Students/Main.java
+++ HW 03b/Surveying Students/Main.java
@@ -1,37 +1,40 @@
+import java.util.*;
 public class Main{
   public static void main(String[] args){
     // Begin set1
+    System.out.println("Set 1:");
     ArrayList<String> set1Names;
-    set1Names = new ArrayList<String>;
+    set1Names = new ArrayList<String>();
     set1Names.add("Bob Scott");
     set1Names.add("Maria Robinson");
     set1Names.add("John Stones");
     set1Names.add("Angela Johnson");
     
     ArrayList<Integer> set1Ages;
-    set1Ages = new ArrayList<Integer>;
+    set1Ages = new ArrayList<Integer>();
     set1Ages.add(10);
     set1Ages.add(11);
     set1Ages.add(9);
     set1Ages.add(13);
     
     ArrayList<String> set1FavClasses;
-    set1FavClasses = new ArrayList<String>;
+    set1FavClasses = new ArrayList<String>();
     set1FavClasses.add("Math");
     set1FavClasses.add("Science");
     set1FavClasses.add("Reading");
     set1FavClasses.add("Writing");
     
     StudentSet set1 = new StudentSet(set1Names,set1Ages,set1FavClasses);
-    System.out.println(set1.toString);
-    System.out.println(set1.info_of_oldest);
+    System.out.println("Set 1 after added student:" + set1.toString());
+    set1.info_of_oldest();
     set1.remove_oldest();
     set1.addStudent("Emily",12,"Math");
-    System.out.println(set1.toString);
+    System.out.println(set1.toString());
     
     // Begin set2
+    System.out.println("Set 2:");
     ArrayList<String> set2Names;
-    set2Names = new ArrayList<String>;
+    set2Names = new ArrayList<String>();
     set2Names.add("Bill Miller");
     set2Names.add("James Williams");
     set2Names.add("Michael Smith");
@@ -40,7 +43,7 @@ public class Main{
     set2Names.add("Emma Wilson");
     
     ArrayList<Integer> set2Ages;
-    set2Ages = new ArrayList<Integer>;
+    set2Ages = new ArrayList<Integer>();
     set2Ages.add(14);
     set2Ages.add(11);
     set2Ages.add(10);
@@ -49,7 +52,7 @@ public class Main{
     set2Ages.add(10);
     
     ArrayList<String> set2FavClasses;
-    set2FavClasses = new ArrayList<String>;
+    set2FavClasses = new ArrayList<String>();
     set2FavClasses.add("Geography");
     set2FavClasses.add("Math");
     set2FavClasses.add("Writing");
@@ -58,10 +61,10 @@ public class Main{
     set2FavClasses.add("Reading");
     
     StudentSet set2 = new StudentSet(set2Names,set2Ages,set2FavClasses);
-    System.out.println(set2.toString);
-    System.out.println(set2.info_of_youngest);
+    System.out.println("Set 2 after added student:" + set2.toString());
+    set2.info_of_youngest();
     set2.remove_youngest();
     set2.addStudent("Andy Jones",15,"Science");
-    System.out.println(set2.toString);
+    System.out.println(set2.toString());
   }// end main
 }// end class
--- HW 03b/Surveying Students/StudentSet.java
+++ HW 03b/Surveying Students/StudentSet.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.*;
 public class StudentSet{
    private ArrayList<String> studentNames;
    private ArrayList<Integer> studentAges;
@@ -11,15 +11,15 @@ public class StudentSet{
    }// end constructor
    
    public void addName(String name){
-      this.studentNames = name;
+      studentNames.add(name);
    }// end addName
    
    public void addAge(int age){
-      this.studentAges = age;
+      studentAges.add(age);
    }// end addAge
    
    public void addFavClass(String favclass){
-      this.favoriteClasses = favclass;
+      favoriteClasses.add(favclass);
    }// end addFavClass
    
    public void addStudent(String student_name, int student_age, String favorite_class){
@@ -28,19 +28,44 @@ public class StudentSet{
       addFavClass(favorite_class);
    }// end addStudent
    
-   /*public void info_of_oldest(){
-      for(int i = 0; i<studentNames.size();i++){
-       System.out.println("The name of the student is: );  
+   public void info_of_oldest(){
+      Collections.sort(studentAges, Collections.reverseOrder());
+      for(int i = 0; i<studentAges.size();i++){
+       System.out.println("The name of the student is: " + studentNames.get(i) + ", the age of the student is: " + studentAges.get(i) +
+       ", and the student's favorite class is: " + favoriteClasses.get(i));  
       }
-   }// end info_of_oldest */
+   }// end info_of_oldest 
    
-   /*public void info_of_youngest(){
-      for(int i = 0; i<studentNames.size();i++){
-       System.out.println("The name of the student is: );  
+   public void remove_oldest(){
+      Collections.sort(studentAges, Collections.reverseOrder());
+      Collections.sort(studentNames, Collections.reverseOrder());
+      Collections.sort(favoriteClasses, Collections.reverseOrder());
+      studentAges.remove(0);
+      studentNames.remove(0);
+      favoriteClasses.remove(0);
+   }// end remove_oldest
+   
+   public void info_of_youngest(){
+      Collections.sort(studentAges);
+      for(int i = 0; i<studentAges.size();i++){
+       System.out.println("The name of the student is: " + studentNames.get(i) + ", the age of the student is: " + studentAges.get(i) +
+       ", and the student's favorite class is: " + favoriteClasses.get(i));  
       }
-   }// end info_of_oldest */
+   }// end info_of_oldest 
+   
+   public void remove_youngest(){
+      Collections.sort(studentAges);
+      Collections.sort(studentNames);
+      Collections.sort(favoriteClasses);
+      studentAges.remove(0);
+      studentNames.remove(0);
+      favoriteClasses.remove(0);
+   }// end remove_oldest
                           
-   public String toString{
+   public String toString(){
+      for(int i = 0; i<studentNames.size();i++){
+       System.out.println("Student's name is " + studentNames.get(i) + ", age is " + studentAges.get(i) + ", favorite class is " + favoriteClasses.get(i));  
+      }
       return "";
    }// end toString
 }
