29e6550f9f2f06954f21837482178c7fdb8e9a6a
kyle <44934418+kwsantiago@users.noreply.github.com>
2019-11-16 14:58:55 -0500

    Update SimpleJSONEditor.java

--- HW 04/Simple JSON Class/SimpleJSONEditor.java
+++ HW 04/Simple JSON Class/SimpleJSONEditor.java
@@ -10,4 +10,25 @@ public class SimpleJSONEditor{
       values.add(fNumber);
    }// end constructor
    
+   public int getValueLabeled(String label){
+      int label1 = 0;
+      for(int i = 0; i<labels.size(); i++){
+         if(labels.get(i) == label){
+            label1 = labels.indexOf(i);
+         }
+      }
+      return values.get(label1);
+   }// end getValueLabeled
+   
+   // finish stuff here // INCOMPLETE STUFF HERE
+   
+   public toString(){
+      for(int i = 0; i<labels.size(); i++){
+         System.out.println(labels.get(i));
+         }
+      for(int i = 0; i<values.size(); i++){
+         System.out.println(values.get(i));
+         }
+   }// end toString
+   
 }// end class
