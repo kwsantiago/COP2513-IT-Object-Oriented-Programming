3a5a59eb54ce0944a70e620382d44e708990fe6c
kyle <44934418+kwsantiago@users.noreply.github.com>
2019-11-16 14:13:44 -0500

    Update ContactList.java

--- HW 04/Contact List with user input & Exceptions/ContactList.java
+++ HW 04/Contact List with user input & Exceptions/ContactList.java
@@ -1,15 +1,15 @@
 import java.util.*;
 public class ContactList{
-   private Contact[] contacts;
+   private ArrayList<Contact> contacts;
    private int size;
    
    ContactList(){
-      contacts = new Contact[5];
+      contacts =  new ArrayList<Contact>(5);
       size = 0;
    }// end no-arg constructor
    
    public boolean full(){
-      if(contacts.length == 5){
+      if(contacts.size() == 5){
          return true;
       }else{
       return false;
@@ -17,7 +17,31 @@ public class ContactList{
    }// end full
    
    public boolean add(Contact obj){
-      size += 1;
-      return false;
+      contacts.add(obj);
+      size ++;
    }// end add
+   
+   public void printAll(){
+      for(int i = 0; i<contacts.size(); i++){
+       System.out.println(contacts.get(i));  
+      }
+   }// end printAll
+   
+   public void search(String LastName){
+      for(int i = 0; i<contacts.size(); i++){
+       if(LastName == lastName){
+         System.out.println(contacts.get(i));  
+       }
+      }
+   }// end search
+   
+   public int searchNumMatches(String LastName){
+      int count = 0;
+      for(int i = 0; i<contacts.size(); i++){
+       if(LastName == lastName){
+         count++;  
+       }
+      }
+      return count;
+   }// end searchNumMatches
 }// end class
