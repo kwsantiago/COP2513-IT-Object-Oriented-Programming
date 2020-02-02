9aca8aab7cbc4aa613568cbfe34de977bd9dbbea
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-02-02 01:06:10 -0500

    Cleanup

--- Practice/Employee/EmployeeNotCloneableException.java
+++
@@ -1,7 +0,0 @@
-import java.io.*;
-import java.util.*;
-public class EmployeeNotCloneableException extends Exception{
-   EmployeeNotCloneableException(String msg){
-      super(msg);
-   }// end validate
-}// end class
--- Practice/TemperatureDataStore/Again/TemperaturesDataStoreException.java
+++
@@ -1,7 +0,0 @@
-import java.io.*;
-import java.util.*;
-public class TemperaturesDataStoreException extends Exception{
-   TemperaturesDataStoreException(String msg){
-      super(msg);
-   }  
-}// end class   
